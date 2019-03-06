d9993e598f3049812570d9cd3da8237bcdcdf339
Sergo <sergo@autobahn.tech>
2019-03-06 13:33:48 +0200

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Typa
+
+Handle all clicks in a single listener
+
+## Install
+
+`yarn add typa`
+
+## Example
+
+```html
+<div class="typa"></div>
+```
+
++
+
+```javascript
+import Typa from 'typa';
+
+const typa = new Typa({
+	strings: ['do', 'your', 'thing'], // The strings to alternate between
+	speed: 100, // The wait time after each letter has been typed
+	delay: 1000, // How long to pause for after a string is complete
+	loop: true, // Whether to start over after the last word in the string array
+	containerSelector: '.typa', // The container selecor. The element must exist beforehand.
+	typerClass: 'typa__typer', // A span with this class will be created in the container and will contain the content
+	cursorClass: 'typa__cursor', // Optional. Will create another span for the blinking cursor, if you wish
+});
+
+typa.start();
+```
---
+++ src/index.ts
@@ -0,0 +1,163 @@
+interface Typa {
+	id: number;
+	opts: {
+		strings: Array<string>;
+		speed: number;
+		delay: number;
+		loop: boolean;
+		containerSelector: string;
+		typerClass: string;
+		cursorClass: string;
+	};
+	container: HTMLElement;
+	timeout: any;
+}
+class Typa {
+	constructor(opts) {
+		this.id = +new Date();
+		this.opts = opts;
+	}
+
+	start() {
+		const {
+			strings,
+			speed,
+			delay,
+			loop,
+			containerSelector,
+			typerClass,
+			cursorClass,
+		} = this.opts;
+
+		this.container = document.querySelector(containerSelector);
+
+		if (this.container) {
+			this.container.innerHTML = `<span class="${typerClass}"></span>${
+				cursorClass ? `<span class="${cursorClass}" />` : ''
+			}`;
+
+			const typer = document.querySelector(`.${typerClass}`);
+			const { strings, speed, delay, loop } = this.opts;
+
+			this.typing(typer, strings, 0, 0, speed, delay, loop);
+		}
+	}
+
+	stop() {
+		clearTimeout(this.timeout());
+	}
+
+	typing(
+		elem: Element,
+		strings: Array<string>,
+		currentLetter,
+		currentWord,
+		speed,
+		delay,
+		loop,
+		reverse?: boolean,
+	) {
+		if (!reverse) {
+			if (strings[currentWord]) {
+				if (currentLetter < strings[currentWord].length) {
+					elem.innerHTML = strings[currentWord].substring(0, currentLetter + 1);
+					currentLetter += 1;
+
+					this.timeout = setTimeout(() => {
+						this.typing(
+							elem,
+							strings,
+							currentLetter,
+							currentWord,
+							speed,
+							delay,
+							loop,
+						);
+					}, speed);
+				} else {
+					this.timeout = setTimeout(() => {
+						this.typing(
+							elem,
+							strings,
+							currentLetter,
+							currentWord,
+							speed,
+							delay,
+							loop,
+							true,
+						);
+					}, delay);
+				}
+			} else {
+				currentWord = 0;
+
+				this.typing(
+					elem,
+					strings,
+					currentLetter,
+					currentWord,
+					speed,
+					delay,
+					loop,
+				);
+			}
+		} else {
+			if (currentLetter > 0) {
+				if (loop) {
+					elem.innerHTML = strings[currentWord].substring(0, currentLetter - 1);
+					currentLetter -= 1;
+
+					this.timeout = setTimeout(() => {
+						this.typing(
+							elem,
+							strings,
+							currentLetter,
+							currentWord,
+							speed,
+							delay,
+							loop,
+							reverse,
+						);
+					}, speed);
+				} else {
+					if (strings.length - 1 !== currentWord) {
+						elem.innerHTML = strings[currentWord].substring(
+							0,
+							currentLetter - 1,
+						);
+						currentLetter -= 1;
+
+						this.timeout = setTimeout(() => {
+							this.typing(
+								elem,
+								strings,
+								currentLetter,
+								currentWord,
+								speed,
+								delay,
+								loop,
+								reverse,
+							);
+						}, speed);
+					}
+				}
+			} else {
+				currentWord += 1;
+
+				this.timeout = setTimeout(() => {
+					this.typing(
+						elem,
+						strings,
+						currentLetter,
+						currentWord,
+						speed,
+						delay,
+						loop,
+					);
+				}, speed);
+			}
+		}
+	}
+}
+
+export default Typa;
